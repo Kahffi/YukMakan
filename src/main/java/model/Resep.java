ed594f7fd8e48a103f825b56c940c20827599f91
fauzan <fauzanhernadi004@gmail.com>
2023-12-02 20:20:09 +0700

    recovery class resep sama daonya yang ketimpa

--- src/main/java/model/Resep.java
+++ src/main/java/model/Resep.java
@@ -4,10 +4,142 @@
  */
 package model;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 /**
  *
  * @author Kahffi
  */
 public class Resep {
+    UUID id;
+    String judul;
+    String deskripsi;
+    String langkah;
+    String bahan;
+    String kandunganGizi;
+    String imagePath;
+    String datePosted;
+    Admin uploader;
+    
+    //Arraylist untuk menyimpan ulasan
+    ArrayList <Ulasan> ulasan = new ArrayList <>();
+
+    // constructor
+    public Resep(UUID id, String judul, String deskripsi, String langkah, String bahan, String kandunganGizi, String imagePath, String datePosted, Admin uploader) {
+        this.id = id;
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.langkah = langkah;
+        this.bahan = bahan;
+        this.kandunganGizi = kandunganGizi;
+        this.imagePath = imagePath;
+        this.datePosted = datePosted;
+        this.uploader = uploader;
+    }
+    
+    
+    public void printResep(){
+        System.out.println("======= " + getJudul() + " =======");
+        System.out.println(getImagePath());
+        System.out.println("Deskripsi: ");
+        System.out.println(getDeskripsi());
+        System.out.println("Bahan: ");
+        System.out.println(getBahan());
+        System.out.println("Langkah: ");
+        System.out.println(getLangkah());
+        System.out.println("Kandungan gizi: ");
+        System.out.println(getKandunganGizi());
+        System.out.println("Diupload oleh: " + getUploader().getUsername() + ", Pada tanggal: " + getDatePosted());
+    }
+    
+    //menambahkan ulasan kedalam resep
+    public void addUlasan(Ulasan u){
+        this.ulasan.add(u);
+    }
+    
+    //setter & getter 
+
+    public ArrayList<Ulasan> getUlasan() {
+        return ulasan;
+    }
+
+    public void setUlasan(ArrayList<Ulasan> ulasan) {
+        this.ulasan = ulasan;
+    }
+    
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public String getLangkah() {
+        return langkah;
+    }
+
+    public void setLangkah(String langkah) {
+        this.langkah = langkah;
+    }
+
+    public String getBahan() {
+        return bahan;
+    }
+
+    public void setBahan(String bahan) {
+        this.bahan = bahan;
+    }
+
+    public String getKandunganGizi() {
+        return kandunganGizi;
+    }
+
+    public void setKandunganGizi(String kandunganGizi) {
+        this.kandunganGizi = kandunganGizi;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getDatePosted() {
+        return datePosted;
+    }
+
+    public void setDatePosted(String datePosted) {
+        this.datePosted = datePosted;
+    }
+
+    public Admin getUploader() {
+        return uploader;
+    }
+
+    public void setUploader(Admin uploader) {
+        this.uploader = uploader;
+    }
+    
+    
     
 }
---
+++ src/main/java/utils/ResepDAO.java
@@ -0,0 +1,129 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Admin;
+
+import model.Resep;
+import model.Ulasan;
+        
+
+/**
+ *
+ * @author Kahffi
+ */
+public class ResepDAO {
+    private static Connection conn;
+    private static ResultSet rs;
+    private static PreparedStatement stmt;
+    
+    
+    public static void saveResep(Resep r){
+        try {
+            String query = "insert into resep values ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')";
+            query = String.format(query, r.getId().toString(), r.getUploader().getUsername(),
+                    r.getJudul(), r.getDatePosted(), r.getDeskripsi(),r.getLangkah(),
+                    r.getBahan(), r.getKandunganGizi(), r.getImagePath());
+            conn = BaseDAO.getConn();
+            System.out.println("ini query : " + query);
+            stmt = conn.prepareStatement(query);
+            stmt.executeUpdate();
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println("Gagal memasukkan data kedalam database");
+            Logger.getLogger(ResepDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    // update resep 
+    public static void updateResep(Resep r){
+        String query = "update resep set judul = '%s', deskripsi = '%s', langkah = '%s', bahan = '%s', kandunganGizi = '%s', imagePath = '%s'  where id  = '%s'";
+        query = String.format(query, r.getJudul(), r.getDeskripsi(),
+                r.getLangkah(), r.getBahan(), r.getKandunganGizi(), r.getImagePath());
+        conn = BaseDAO.getConn();
+        try {
+            stmt = conn.prepareStatement(query);
+            stmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(KontenEduDAO.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("update gagal");
+        }
+    }
+    
+    //hapus resep (dan juga ulasan yang berkaitan)
+    public static void delResep(Resep r){
+        String query = "DELETE FROM yukmakan.resep WHERE id = '%s';";
+        query = String.format(query, r.getId().toString());
+        conn = BaseDAO.getConn();
+        try {
+            stmt = conn.prepareStatement(query);
+            stmt.executeUpdate();
+            
+            //sambungkan dengan method pada ulasanDAO untuk menghapus ulasan
+            //ulasanDAO.delUlasan(r.getId().toString());
+            
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ResepDAO.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Hapus data gagal");
+        }
+    }
+    
+    // mengambil data dari database berdasarkan id resep
+    public Resep getResep (String id){
+        Resep r;
+        String query = "Select * from resep where id = '%s'";
+        query = String.format(query, id);
+        try {
+            conn = BaseDAO.getConn();
+            stmt = conn.prepareStatement(query);
+            rs = stmt.executeQuery();
+            
+            if (!rs.next()){
+                Admin admin;
+                UUID uuid;
+                ArrayList <Ulasan> ulasan = new ArrayList <>();
+                String imagePath, judul, tanggal, uploader, deskripsi, bahan, kandunganGizi, langkah;
+                
+                uuid = UUID.fromString(rs.getString(1));
+                uploader = rs.getString(2);
+                judul = rs.getString(3);
+                tanggal = rs.getString(4);
+                deskripsi = rs.getString(5);
+                langkah = rs.getString(6);
+                bahan = rs.getString(7);
+                kandunganGizi = rs.getString(8);
+                imagePath = rs.getString(9);
+                
+                // mengambil data admin untuk construct class resep
+                admin = AkunDAO.getAdmin(uploader);
+                //ulasan = UlasanDAO.getUlasan(uuid.toString());
+                r = new Resep (uuid, judul, deskripsi, langkah, bahan, kandunganGizi, imagePath, tanggal,admin);
+                
+                conn.close();
+                return r;
+            }
+            else{
+                System.out.println("gagal mengambil data konten edukasi");
+                return null;
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(KontenEduDAO.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("gagal mengambil data konten edukasi");
+            return null;
+        }
+        
+    }
+    
+}
